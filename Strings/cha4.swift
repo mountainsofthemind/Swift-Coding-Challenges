a8e598e12883234247d5b3ec5a8b36d0427e592a
mountainsofthemind <contact@nathanvisser.dev>
2021-04-09 07:27:11 -0400

    Altered challenge 4 removing deprecated solution.
    The following solution to challenge 4 has been deprecated
    & therefor removed:
    extension String {
    func stringContains(string: String) -> Bool {
    return self.uppercased().range(of: string.uppercased()) != nil
    }}

--- Strings/cha4.swift
+++ Strings/cha4.swift
@@ -5,29 +5,14 @@ ignores letter case, & without using the existing contains() method
 */
 
 extension String {
-func challenge4a(string: String) -> Bool{
+func challenge4(string: String) -> Bool{
 }}
 
-extension String {
-func challenge4b(string: String) -> Bool {
-}}
-
-
-assert("Hello, world".challenge4a(string:"Hello") == true, "test 1 failed")
+assert("Hello, world".challenge4(string:"Hello") == true, "test 1 failed")
 print("test 1 passed")
-assert("Hello, world".challenge4a(string: "WORLD") == true, "test 2 failed")
+assert("Hello, world".challenge4(string: "WORLD") == true, "test 2 failed")
 print("test 2 passed")
-assert("Hello, world".challenge4a(string: "Goodbye") == false, "test 3 failed")
+assert("Hello, world".challenge4(string: "Goodbye") == false, "test 3 failed")
 print("test 3 passed")
-print("challenge 4 A passed")
-
-
-assert("Hello, world".challenge4b(string:"Hello") == true, "test 1 failed")
-print("test 1 passed")
-assert("Hello, world".challenge4b(string: "WORLD") == true, "test 2 failed")
-print("test 2 passed")
-assert("Hello, world".challenge4b(string: "Goodbye") == false, "test 3 failed")
-print("test 3 passed")
-print("challenge 4 B passed")
 print("giddy upp cowboy! You passed!")
 
